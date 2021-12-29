38ad447a0eaf9cc3aea45461f381704dc359a081
Nathan Skiles <nateskiles94@gmail.com>
2021-12-28 22:06:29 -0800

    generators init

---
+++ es6JavaScript/js_files/for_of_loops.js
@@ -0,0 +1,12 @@
+const colors = ['red', 'green', 'blue'];
+
+for (const color of colors) {
+    console.log(color);
+}
+
+const numbers = [1, 2, 3, 4, 5];
+
+let total = 0;
+for (const number of numbers) {
+    console.log(total += number);
+}
