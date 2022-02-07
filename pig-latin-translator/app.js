ed423610c6b95284ce8ab1a9bd58e630bcaa87a7
Nathan Skiles <nateskiles94@gmail.com>
2022-02-07 00:30:35 -0800

    refactor

--- pig-latin-translator/app.js
+++ pig-latin-translator/app.js
@@ -7,9 +7,9 @@ let isVowel = char => {
     return vowels.indexOf(char.toLowerCase()) !== -1;
 }
 
-let capitalizeFirstLetter = string => string.charAt(0).toUpperCase() + string.slice(1);
+let upperFirstLetter = str => str.charAt(0).toUpperCase() + str.slice(1);
 
-let removeLastChar = string => string.slice(0, -1);
+let removeLastChar = str => str.slice(0, -1);
 
 // Logic
 // Return an array of translated words
@@ -33,15 +33,15 @@ let translator = input => {
     let translation = output(words);
     let string = "";
 
-    translation.forEach(word => string += `${capitalizeFirstLetter(word)} `);
+    translation.forEach(word => string += `${upperFirstLetter(word)} `);
     return `Original: "${input}"\nTranslation: "${removeLastChar(string)}"\n`;
 }
 
 // App
 let app = () => {
     while (true) {
-        let input = prompt(`Please enter a word or phrase you'd like translate: `).toLowerCase();
-        if (input === 'quit' || input === 'q') {
+        let input = prompt(`Please enter a word or phrase you'd like translate: `);
+        if (input.toLowerCase() === 'quit' || input.toLowerCase() === 'q') {
             return false;
         } else {
             console.log(`${translator(input)}\n\nTo stop, please type quit or q\n\n`);
