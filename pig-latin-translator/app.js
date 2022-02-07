2de3d2922366ba9e3c40cfcd23fc62b71b3ec09c
Nathan Skiles <nateskiles94@gmail.com>
2022-02-07 00:26:19 -0800

    refactor

--- pig-latin-translator/app.js
+++ pig-latin-translator/app.js
@@ -1,5 +1,6 @@
 const prompt = require('prompt-sync')();
 
+// Util functions
 // Returns true if the given character is a vowel
 let isVowel = char => {
     const vowels = ['a', 'e', 'i', 'o', 'u'];
@@ -10,10 +11,10 @@ let capitalizeFirstLetter = string => string.charAt(0).toUpperCase() + string.sl
 
 let removeLastChar = string => string.slice(0, -1);
 
+// Logic
 // Return an array of translated words
-let translator = words => {
-    let translation = [];
-    words.forEach(word => {
+let output = words => {
+    return words.map(function (word) {
         word += "-";
         let firstLetter = word.charAt(0);
         if (isVowel(firstLetter)) {
@@ -23,28 +24,27 @@ let translator = words => {
             word += "ay";
             word = word.slice(1);
         }
-        translation.push(word);
-    });
-    return translation;
+        return word
+    })
 }
 
-let output = words => {
-    let translation = translator(words);
-    let string1 = "";
-    let string2 = "";
-    words.forEach(word => string1 += `${capitalizeFirstLetter(word)} `);
+let translator = input => {
+    let words = input.split(' ');
+    let translation = output(words);
+    let string = "";
 
-    translation.forEach(word => string2 += `${capitalizeFirstLetter(word)} `);
-    return `Original: "${removeLastChar(string1)}"\nTranslation: "${removeLastChar(string2)}"\n`;
+    translation.forEach(word => string += `${capitalizeFirstLetter(word)} `);
+    return `Original: "${input}"\nTranslation: "${removeLastChar(string)}"\n`;
 }
 
+// App
 let app = () => {
     while (true) {
         let input = prompt(`Please enter a word or phrase you'd like translate: `).toLowerCase();
-        if (input === 'quit' || input === 'q' ) {
+        if (input === 'quit' || input === 'q') {
             return false;
         } else {
-            console.log(`${output(input.split(' '))}\n\nTo stop, please type quit or q\n\n`);
+            console.log(`${translator(input)}\n\nTo stop, please type quit or q\n\n`);
         }
     }
 }
